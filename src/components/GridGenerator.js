48d715e52a26ab2275dc7ff433c592df10b093cc
Juan Sebastián Castaño Ramírez <jscr00@hotmail.com>
2022-11-13 11:34:40 -0500

    feat: maquetación acabada

--- src/components/GridGenerator.js
+++ src/components/GridGenerator.js
@@ -1,14 +1,21 @@
 import React from "react";
+import "../styles/components/grid.css";
 
 const GridGenerator = () => {
     return (
-        <div style={{ paddingTop: 2000, paddingBottom: 2000 }}>
-            <div className="row">
-                <div className="col-12">
-                    <h2>Grid</h2>
-                </div>
-            </div>
-            <div className="row"></div>
+        <div className="grid-container">
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
         </div>
     );
 };
